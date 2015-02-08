d906587fe6ad48c1ca328835735553428c7cfcad
Teslim <Teslim@n2-102-138.dhcp.drexel.edu>
2015-02-08 15:29:35 -0500

    test commit

---
+++ Build_one/Assets/scripts/TileSwitch.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TileSwitch : MonoBehaviour {
+
+
+	public enum TileDirection{Up, Down, Left, Right};
+    public TileDirection currentDirection;
+	
+	Texture2D tex = Resources.Load("forward_tex.jpg") as Texture2D;
+
+			
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	void Update(){
+
+	}
+
+
+	void OnMouseDown (){
+		UpdateCurrentDirection();
+	}
+	// Update is called once per frame
+	void UpdateCurrentDirection () {
+	
+		bool isValid = true;
+
+		do {
+	   
+						switch (currentDirection) {
+			
+						case(TileDirection.Up):
+				        transform.renderer.material.mainTexture = tex;
+								currentDirection ++;
+								break;
+						case(TileDirection.Down):
+								transform.renderer.material.color = Color.green;
+								currentDirection ++;
+								break;
+
+						case(TileDirection.Left):
+								transform.renderer.material.color = Color.cyan;
+								currentDirection ++;
+								break;
+						case(TileDirection.Right):
+								transform.renderer.material.color = Color.magenta;
+								currentDirection ++;
+				                currentDirection = 0;
+								break;
+						}
+				} while(isValid != true);
+				
+		}
+		
+
+
+}
+
+
+
+		
+			
+		 
+		
+	
+
